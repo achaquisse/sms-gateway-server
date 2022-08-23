3abcc4f7728a985afc85a6dc03273eaf1f1e7a7a
Americo Chaquisse <achaquisse1@gmail.com>
2022-08-23 18:49:52 +0200

    update lambda architecture

--- infra/lambda_update_status.tf
+++ infra/lambda_update_status.tf
@@ -4,9 +4,9 @@ resource "aws_lambda_function" "lambda_update_status" {
   role          = aws_iam_role.lambda_exec_iam_role.arn
   handler       = "update-status"
 
-  runtime     = "go1.x"
-  memory_size = 128
-  timeout     = 10
+  runtime       = "go1.x"
+  memory_size   = 128
+  timeout       = 10
   architectures = ["arm64"]
 
   environment {
